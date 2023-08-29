6ac940c22842beece2d6072870f7dec4668c00a6
Maria Okatova <mariia.okatova@volkswagen.de>
2023-08-29 11:28:52 +0200

    add add-to-cart button

--- frontend/src/app/book-details/book-details.component.ts
+++ frontend/src/app/book-details/book-details.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Book} from "../model/Book";
 import {ActivatedRoute} from "@angular/router";
 import {FetchedBooksService} from "../service/fetched-books.service";
+import {CartService} from "../service/cart.service";
 
 @Component({
   selector: 'app-book-details',
@@ -12,10 +13,16 @@ export class BookDetailsComponent {
   book: Book | undefined;
   books: Book[] = [];
 
-  constructor(private route: ActivatedRoute, private fetchedBookService: FetchedBooksService) {
+  constructor(private route: ActivatedRoute,
+              private fetchedBookService: FetchedBooksService,
+              private cartService: CartService) {
     this.books = fetchedBookService.getBooks();
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('bookId'));
     this.book = this.books.find(book => book.id === productIdFromRoute);
+  }
+
+  public addToCart(book: Book) {
+    this.cartService.addToCart(book);
   }
 }
