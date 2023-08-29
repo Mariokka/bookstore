e36bff2cf55fd924611ed0629c40837a162fd874
Maria Okatova <mariia.okatova@volkswagen.de>
2023-08-29 10:03:45 +0200

    allows to buy AND click to see the details

--- frontend/src/app/book-details/book-details.component.ts
+++ frontend/src/app/book-details/book-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Book} from "../model/Book";
+import {ActivatedRoute} from "@angular/router";
+import {BooksService} from "../service/books.service";
 
 @Component({
   selector: 'app-book-details',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-details.component.css']
 })
 export class BookDetailsComponent {
+  book: Book | undefined;
+  books: Book[] = [];
 
+  // ngOnInit() {
+  //   const routeParams = this.route.snapshot.paramMap;
+  //   const productIdFromRoute = Number(routeParams.get('bookId'));
+  //
+  //   this.book = books.find(book => book.id === productIdFromRoute);
+  // }
+  // constructor(private route: ActivatedRoute, private bookService: BooksService) {
+  //   this.books = bookService.
+  // }
 }
